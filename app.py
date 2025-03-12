586ccf9015fef9c21a1ca375369fbe55ebf4cb86
Meghana <meghanamaggie0987@gmail.com>
2025-03-12 21:24:34 +0530

    first commit

--- app.py
+++ app.py
@@ -28,20 +28,25 @@ outlet_location_type_tier1 = st.selectbox('Outlet Location Type Tier 1', [0, 1])
 outlet_type_supermarket_type1 = st.selectbox('Outlet Type Supermarket Type1', [0, 1])
 item_category_fd = st.selectbox('Item Category FD', [0,1])
 
-# Create a DataFrame from the input values
-input_data = pd.DataFrame({
-    'Item_Weight': [item_weight],
-    'Item_Visibility': [item_visibility],
-    'Item_MRP': [item_mrp],
-    'Outlet_Age': [outlet_age],
-    'Item_Fat_Content_Low Fat': [item_fat_content_low_fat],
-    'Item_Type_Dairy': [item_type_dairy],
-    'Item_Type_Soft Drinks': [item_type_softdrinks],
-    'Outlet_Size_Medium': [outlet_size_medium],
-    'Outlet_Location_Type_Tier 1': [outlet_location_type_tier1],
-    'Outlet_Type_Supermarket Type1': [outlet_type_supermarket_type1],
-    'Item_Category_FD': [item_category_fd]
-})
+#After training your model.
+    trained_columns = X_train.columns.to_list()
+
+    #Then when creating your input data.
+    input_data = pd.DataFrame({
+        'Item_Weight': [item_weight],
+        'Item_Visibility': [item_visibility],
+        'Item_MRP': [item_mrp],
+        'Outlet_Age': [outlet_age],
+        'Item_Fat_Content_Low Fat': [item_fat_content_low_fat],
+        'Item_Type_Dairy': [item_type_dairy],
+        'Item_Type_Soft Drinks': [item_type_softdrinks],
+        'Outlet_Size_Medium': [outlet_size_medium],
+        'Outlet_Location_Type_Tier 1': [outlet_location_type_tier1],
+        'Outlet_Type_Supermarket Type1': [outlet_type_supermarket_type1],
+        'Item_Category_FD': [item_category_fd]
+    })
+
+    input_data = input_data[trained_columns]
 
 # Scale numerical features
 numerical_features = ['Item_Weight', 'Item_Visibility', 'Item_MRP', 'Outlet_Age']
